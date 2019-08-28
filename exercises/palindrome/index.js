161732c3742add3a406e0370ce9507ffbcbae8f9
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-08-28 23:48:36 +0100

    Add comments to alternate solution for palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -37,7 +37,26 @@
 
 function palindrome(str) {
   return str.split("").every((char, i) => char === str[str.length - i - 1])
-  // this is a less preferable solution because it has double comparison,
+  // The every method goes through all elements of an array and returns true
+  // if the condition in the anon function is met for each element (& false
+  // if not)
+
+  // Here the condition is that each character must be the same as it's
+  // mirror image character, i.e. the character the same number of positions
+  // away from the end as it, but in the other direction.
+
+  // This mirror character can be picked out by finding the element
+  // in the original string with the correct index. The index can be found by
+  // finding the length of the original string, then subtracting the index of
+  // the other character and adding one. E.g. if we are comparing char at index
+  // 0 with the string 'hello' this is how we would find the index of the mirror
+  // char: the string length is 5 so minus the original index from this, i.e. 1,
+  // then minus one again (this needs to be done as array index starts at 0)
+
+  // Now you have the index of the mirror letter it can easily be found and
+  // compared with its corresponding letter
+
+  // Note: this is a less preferable solution because it has double comparison,
   // it compares the first to the last and the last to the first etc.
   // so it is doing twice as much as it needs to do
 }
