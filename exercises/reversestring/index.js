b0ca2ae1b51638b9c0aaf1d93ca633d00f71aece
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-08-28 15:41:29 +0100

    Solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,10 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  const arr = str.split("")
 
-module.exports = reverse;
+  return arr.reverse().join("")
+}
+
+module.exports = reverse
