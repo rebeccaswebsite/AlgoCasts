5ee5f5ea86626812c56d25491400da189c73c2d8
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-08-28 17:13:27 +0100

    Reversed string third solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,14 +7,28 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversed = ""
-
-  for (let character of str) {
-    reversed = character + reversed
-  }
-
-  return reversed
+  return str.split("").reduce((rev, char) => char + rev, "")
+  // Split string into array
+  // Use reduce method: use callback function and initial value of
+  // an empty string. 'rev' is the accumulator, the collection of the
+  // iterations, 'char' is the individual element currently being
+  // iterated on. This array method is to reduce an array down, in
+  // this case, we are reducing it into a string, with the characters
+  // reversed. For each char, char is added to rev value- which begins
+  // as an empty string. The new char is concatenated to the beginning
+  // of the string, which means that once all the chars have been
+  // iterated over, the value of rev will be the reversed string.
 }
+
+// function reverse(str) {
+//   let reversed = ""
+
+//   for (let character of str) {
+//     reversed = character + reversed
+//   }
+
+//   return reversed
+// }
 
 // function reverse(str) {
 //   return str
