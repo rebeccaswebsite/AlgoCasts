55e2c39755ed04e5d744914d5ede3d599c9b1ce4
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-08-28 23:10:22 +0100

    Add comments to recursive reverse integer solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const chars = String(n)
+  // turn int into string
+  const reversedChars = chars.split("").reduce((accumulator, element) => {
+    return element + accumulator
+  }, "")
+  // split string into an array, use reduce to reverse this array
+  // start with initial empty string and then add first value of the array to the
+  // accumulator. Assign the reversed string which is returned to reversedChars
 
-module.exports = reverseInt;
+  return parseInt(reversedChars) * Math.sign(n)
+  // turn reversedChars from integer to string
+  // use Math.sign(n) to maintain the original mathematical sign (e.g. negative)
+}
+
+module.exports = reverseInt
