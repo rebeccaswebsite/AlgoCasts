c90ddfb15e4145c9fb71ad6c496858c973d59c3b
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-09-03 16:01:40 +0100

    Solution for chunk using slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,25 +8,45 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
+function chunk(arr, size) {
   const chunked = []
   // create empty array to hold 'chunked' version of our data
+  let index = 0
+  // create index with value of 0, it is a variable we want to
+  // allow to change over time so we use let
 
-  for (let el of array) {
-    // for each element in the 'unchunked' array
-    const last = chunked[chunked.length - 1]
-    // retreive the last element in 'chunked'
-    if (!last || last.length === size) {
-      // if that element does not exist, or if length is equal to chunk size
-      chunked.push([el])
-      // push a new chunk (an array) with the current element into chunked
-    } else {
-      last.push(el)
-      // otherwise, add the current element into the chunk
-    }
+  while (index < arr.length) {
+    // while index is less than the length of the original array
+    chunked.push(arr.slice(index, index + size))
+    // push the sliced array ('chunk') into the empty chunked array
+    index += size
+    // add size to index variable, to take scoops through the original
+    // array with each iteration
   }
 
   return chunked
+  // return the chunked array
 }
+
+// function chunk(arr, size) {
+//   const chunked = []
+//   // create empty array to hold 'chunked' version of our data
+
+//   for (let el of arr) {
+//     // for each element in the 'unchunked' array
+//     const last = chunked[chunked.length - 1]
+//     // retreive the last element in 'chunked'
+//     if (!last || last.length === size) {
+//       // if that element does not exist, or if length is equal to chunk size
+//       chunked.push([el])
+//       // push a new chunk (an array) with the current element into chunked
+//     } else {
+//       last.push(el)
+//       // otherwise, add the current element into the chunk
+//     }
+//   }
+
+//   return chunked
+// }
 
 module.exports = chunk
